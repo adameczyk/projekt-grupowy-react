ade9890380505a3efad27e81a9fe8ed892db5c96
adameczyk <adameczyk123@gmail.com>
2025-11-13 13:49:31 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div className="bg-amber-900">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
